0b3aa686081cbdbeaf418eb4d85d1603a6dc577f
William M. Stewart III <wmstewart.iii@gmail.com>
2023-02-16 01:36:18 -0500

    use basic implementation of Modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Modal } from 'components/Modal/Modal';
+import { useModal } from 'hooks/useModal';
 import './App.css';
 
-const App = () => <div>Test</div>;
+const App = () => {
+  const { isOpen, toggleModal } = useModal();
+
+  return (
+    <div>
+      <h1>OnsiteIQ HR Application</h1>
+      <button type="button" onClick={toggleModal}>
+        Start a New Review
+      </button>{' '}
+      {/** add icons to buttons */}
+      <button type="button">View Completed Reviews</button> {/** this may make sense to open a separate page */}
+      <Modal isOpen={isOpen}>
+        <button type="button">Approve Candidate</button>
+        <button type="button">Reject Candidate</button>
+      </Modal>
+    </div>
+  );
+};
 
 export default App;
