0568e136c0230ebcccdf98cf2e352e4b4611d942
William M. Stewart III <wmstewart.iii@gmail.com>
2023-02-16 01:35:43 -0500

    update index to react 18 syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!); // the non-null assertion is recommended by the React team for this case (https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis)
+root.render(<App />);
